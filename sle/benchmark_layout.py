267381cd1c793dec6111b61f1357f4a3921cc1b6
CZKQH <2060242710@qq.com>
2026-09-09 19:41:07 +0800

    Add neuroscience spike history candidate
    # Conflicts:
    #	TASKS.md
    #	tests/test_task_cards.py

--- sle/benchmark_layout.py
+++ sle/benchmark_layout.py
@@ -14,6 +14,7 @@ DISCIPLINE_DOMAINS: dict[str, tuple[str, ...]] = {
         "Biomechanics",
         "Ecology",
         "EvidenceSynthesis",
+        "Neuroscience",
         "PopulationGenetics",
         "ProteinEngineering",
         "RNAEngineering",
